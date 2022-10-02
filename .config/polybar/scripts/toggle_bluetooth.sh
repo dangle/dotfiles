610935b311aa8ad3cf33236f489612ba614a85bb
Melissa Nuño <dangle@users.noreply.github.com>
2022-10-02 12:02:42 -0600

    Update toggle_bluetooth.sh

--- .config/polybar/scripts/toggle_bluetooth.sh
+++ .config/polybar/scripts/toggle_bluetooth.sh
@@ -3,9 +3,9 @@
 if [[ $(pactl get-default-sink) != *QEMU* ]]
 then
     pactl set-default-sink alsa_output.usb-QEMU_QEMU_USB_Audio_1-0000_00_02.1_00.0-2-00.analog-stereo
-    bluetoothctl disconnect F8:4E:17:8D:1F:F9
+    bluetoothctl disconnect 88:C9:E8:4F:EC:B0
 else
     bluetoothctl power on
-    bluetoothctl connect F8:4E:17:8D:1F:F9
+    bluetoothctl connect 88:C9:E8:4F:EC:B0
     pactl set-default-sink bluez_sink.F8_4E_17_8D_1F_F9.a2dp_sink
 fi
