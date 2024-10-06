dad31ad1c894b4572e0b8236b2e3bff3422ea3af
Melissa Nuño <dangle@users.noreply.github.com>
2024-10-05 22:29:42 -0600

    Decrease wezterm font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ local config = {}
 config.color_scheme = 'Bright (base16)'
 config.window_background_opacity = 0.7
 config.font = wezterm.font 'Fira Code'
-config.font_size = 15
+config.font_size = 14
 
 config.hide_tab_bar_if_only_one_tab = true
 
