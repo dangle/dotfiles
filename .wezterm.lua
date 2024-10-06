3eab71ab8068fd8d1fb15d64373623c5131b27ee
Melissa Nuño <dangle@users.noreply.github.com>
2024-10-05 18:51:17 -0600

    Add simple wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Bright (base16)'
+config.window_background_opacity = 0.7
+config.font = wezterm.font 'Fira Code'
+config.font_size = 13
+
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
